4a171d74609cc413423f51f3fdc7d5e682382ed4
M Ahmed Butt <144407474+Ahmed8881@users.noreply.github.com>
2024-01-23 19:15:37 +0500

    Add files via upload

---
+++ OOP/LAB 1/Task 3/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace practice2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            students a= new students();
+            Console.WriteLine(a.sname);
+            Console.WriteLine(a.matricMarks);
+            Console.WriteLine(a.fscMarks);
+            Console.WriteLine(a.ecatMarks);
+            Console.WriteLine(a.aggregate);
+            Console.ReadKey();
+           
+        }
+    }
+}
---
+++ OOP/LAB 1/Task 3/students.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace practice2
+{
+    internal class students
+    {
+        //constructor
+       
+        public string sname;
+        public float matricMarks;
+        public float fscMarks;
+        public float ecatMarks;
+        public float aggregate;
+    }
+}
