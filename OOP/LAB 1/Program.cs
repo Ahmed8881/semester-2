6230bcbf783d4016fbe0a436cfd29ceee32c6a08
M Ahmed Butt <144407474+Ahmed8881@users.noreply.github.com>
2024-01-23 19:11:59 +0500

    Add files via upload

---
+++ OOP/LAB 1/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Student[] studentData=new Student[5];
+            for(int i = 0; i < 5; i++)
+            {
+                studentData[i]= takeStudentInput();
+            }
+            printStudentData(studentData);
+            Console.Read();
+        }
+        static Student takeStudentInput() {
+            Student s = new Student();
+            Console.Write("Enter your name: ");
+            s.sname = Console.ReadLine();
+            Console.Write("Enter your matric marks: ");
+            s.matricMarks=float.Parse(Console.ReadLine());
+            Console.Write("Enter your fsc marks: ");
+            s.fscMarks=float.Parse(Console.ReadLine());
+            Console.Write("Enter your ecat marks: ");
+            s.ecatMarks=float.Parse(Console.ReadLine());
+            Console.WriteLine("              ");
+            return s;
+        }
+        static void printStudentData(Student[] studentData) {
+            Console.WriteLine("");
+            Console.WriteLine("Name \t Matric \t Fsc \t Ecat ");
+            for(int i=0;i<5;i++)
+            {
+                Console.WriteLine(studentData[i].sname + "\t" + studentData[i].matricMarks + "\t" + studentData[i].fscMarks + "\t" + studentData[i].ecatMarks);
+            }
+                }
+    }
+}
---
+++ OOP/LAB 1/Student.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Student
+    {
+        public string sname;
+        public float matricMarks;
+        public float fscMarks;
+        public float ecatMarks;
+        public float aggregate;
+
+    }
+}
