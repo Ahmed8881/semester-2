640f8a0889126fc9e21cdf9285cabb06b78eba5e
M Ahmed Butt <144407474+Ahmed8881@users.noreply.github.com>
2024-01-23 19:13:19 +0500

    Add files via upload

---
+++ OOP/LAB 1/task 1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace paraCanstructortask1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            student s1=new student("Ahmed",500,500,400);
+            student s2 = new student("Ali", 550, 550, 390);
+            Console.WriteLine(s1.sname + " \t" + s1.matricMarks + "\t " + s1.fscmarks + "\t " + s1.ecatMarks);
+            Console.WriteLine(s2.sname + " \t" + s2.matricMarks + "\t " + s2.fscmarks + " \t" + s2.ecatMarks);
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ OOP/LAB 1/task 1/student.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Authentication.ExtendedProtection.Configuration;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace paraCanstructortask1
+{
+
+
+    
+
+    internal class student
+    {
+    public student(string name, float matric, float fsc, float ecat)
+    {
+        sname = name;
+        matricMarks = matric;
+        fscmarks = fsc;
+        ecatMarks = ecat;
+    }
+        public string sname;
+        public float matricMarks;
+        public float fscmarks;
+        public float ecatMarks;
+
+
+    }
+}
