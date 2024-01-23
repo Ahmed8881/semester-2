abec703f40a8c59772d0c558e196bdbc2deb1d72
M Ahmed Butt <144407474+Ahmed8881@users.noreply.github.com>
2024-01-23 19:14:30 +0500

    Add files via upload

---
+++ OOP/LAB 1/task 2/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace paramereizedConstructor
+{
+    internal class Program
+    {
+       ;
+
+        static void Main(string[] args)
+        {
+
+            student s1 = new student("Ahmed");
+            student s2 = new student("Ali");
+        }
+    }
+}
---
+++ OOP/LAB 1/task 2/student.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace paramereizedConstructor
+{
+    internal class student
+    {
+        public student(string n) {
+            sname=n;
+        }
+         
+        public string sname;
+        public float matricMarks;
+        public float fscmarks;
+        public float ecatmarks;
+
+    }
+}
