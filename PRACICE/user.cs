c410350cbf0608cdeaafa0e7c5850b27c1546e4f
M Ahmed Butt <144407474+Ahmed8881@users.noreply.github.com>
2024-02-01 06:42:48 +0500

    Add files via upload

---
+++ PRACICE/user.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace appsingn
+{
+   
+    internal class user
+    {
+        public user(string name, string password, string role)
+        {
+            this.name = name;
+            this.password = password;
+            this.role = role;
+
+        }
+        string name;
+        string password;
+        string role;
+    }
+}
