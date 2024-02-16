72d79dc2b66410f5e8258986a247126a0b4ee87d
M Ahmed Butt <144407474+Ahmed8881@users.noreply.github.com>
2024-02-16 08:52:58 +0500

    Add files via upload

---
+++ PD/pd 3/Shirito/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shiritori
+{
+    internal class Program
+    {
+        public Shiritori s1 = new Shiritori();
+        static void Main(string[] args)
+        {
+            Shiritori myShiritori = new Shiritori();
+
+            while (true)
+            {
+                Console.WriteLine("Enter a word (or 'exit' to quit):");
+                string input = Console.ReadLine();
+
+                if (input.ToLower() == "exit")
+                {
+                    break;
+                }
+
+                myShiritori.Play(input);
+            }
+        }
+    }
+  }
+
+
---
+++ PD/pd 3/Shirito/Shiritori.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shiritori
+{
+    internal class Shiritori
+    {
+        private List<string> words;
+        private bool game_over;
+
+        public Shiritori()
+        {
+            words = new List<string>();
+            game_over = false;
+        }
+
+        public bool Play(string word)
+        {
+            if (game_over)
+            {
+                Console.WriteLine("Game over!");
+                return false;
+            }
+
+            if (!words.Contains(word))
+            {
+                words.Add(word);
+                Console.WriteLine($"Valid play! Current words: [{string.Join(", ", words)}]");
+                return true;
+            }
+            if (words.Count == 0 || (!words.Contains(word) && words[words.Count - 1][words[words.Count - 1].Length - 1] == word[0]))
+            {
+                words.Add(word);
+                Console.WriteLine($"Valid play! Current words: [{string.Join(", ", words)}]");
+                return true;
+            }
+            else
+            {
+                Console.WriteLine("Invalid play!");
+                return false;
+            }
+
+        }
+
+        public string Restart()
+        {
+            words.Clear();
+            game_over = false;
+            return "Game restarted!";
+        }
+    }
+}
