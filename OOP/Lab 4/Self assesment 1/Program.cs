2ef13859392887deaaa5908b97b08ee73e655c39
M Ahmed Butt <144407474+Ahmed8881@users.noreply.github.com>
2024-02-18 20:22:20 +0500

    Add files via upload

---
+++ OOP/Lab 4/Self assesment 1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Self_assessment_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Student student = new Student();
+            student.name = "Ahmed";
+            student.rollnum = 18;
+            student.cgpa = 3.9F;
+            student.MatricMarks = 1096;
+            student.fscMarks = 1031;
+            student.EcatMarks = 312;
+            student.homeTown = "Lahore";
+            student.isHostalite = true;
+            student.isTakingScholarship = true;
+            float meritPer = student.calculateMerit();
+            bool isEligible = student.IsEligibleforScholarship(meritPer);
+            Console.WriteLine("Merit percentage "+meritPer);
+            Console.WriteLine("Is Eligible for scholarshiip " + isEligible);
+
+        }
+    }
+}
