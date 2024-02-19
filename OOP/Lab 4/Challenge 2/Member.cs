3df5714050a60c6291ce93836d086e3978944a09
M Ahmed Butt <144407474+Ahmed8881@users.noreply.github.com>
2024-02-19 22:06:40 +0500

    Add files via upload

---
+++ OOP/Lab 4/Challenge 2/Member.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Challenge_2
+{
+    internal class Member
+    {
+        public Member(string name,string memberId) { 
+            this.name = name;
+            this.memberId = memberId;
+            books = new List<string>();
+
+        }
+        public string name;
+        public string memberId;
+        public List<string> books;
+        public int bookbought;
+        public float moneyInBank;
+        public float amountSpent;
+        public string showName()
+        {
+            return name;
+        }
+        public string modifyname(string newName)
+        {
+            name = newName;
+            return name;
+        }
+        public string setName(string addname)
+        {
+            name=addname;
+            return name;
+        }
+        public void ShowStats()
+        {
+            Console.WriteLine("Member ID: " + memberId);
+            Console.WriteLine("Name: " + name);
+            Console.WriteLine("Number of Books Bought: " + bookbought);
+            Console.WriteLine("Amount Spent: " + amountSpent);
+        }
+        public void BuyBook(string bookName, int price)
+        {
+            Console.WriteLine("Buying book: " + bookName);
+            books.Add(bookName);
+            bookbought++;
+            moneyInBank -= price;
+            amountSpent += price;
+        }
+        public void ShowBooksBought()
+        {
+            Console.WriteLine("Books Bought:");
+            for (int i = 0; i < books.Count; i++)
+            {
+                Console.WriteLine(books[i]);
+            }
+        }
+
+
+
+    }
+
+}
---
+++ OOP/Lab 4/Challenge 2/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Challenge_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Member member = new Member("Ahmed", "1");
+            while (true)
+            {
+                Console.WriteLine("1. Set Name");
+                Console.WriteLine("2. Modify Name");
+                Console.WriteLine("3. Buy Book");
+                Console.WriteLine("4. Show Books Bought");
+                Console.WriteLine("5. Show Stats");
+                Console.WriteLine("6. Exit");
+                Console.Write("Enter your choice: ");
+                string choice=Console.ReadLine();
+                if (choice == "1") { 
+                    
+                    SetName(member);
+                }
+                else if (choice == "2") { 
+                    ModifyName(member);
+                }
+                else if (choice == "3") {
+                    BuyBooks(member);
+                }
+                else if (choice == "4") {
+                    member.ShowBooksBought();
+                }
+                else if (choice == "5") {
+                    member.ShowStats();
+                }
+                else if (choice == "6") { }
+                else
+                {
+                    Console.WriteLine("Invalid choice......");
+                }
+
+
+            }
+    
+        }
+        static void SetName(Member member)
+        {
+            Console.Clear();
+            Console.WriteLine("Enter your name: ");
+            string name=Console.ReadLine();
+            member.setName(name);
+        }
+        static void ModifyName(Member member)
+        {
+            Console.Clear();
+            Console.Write("Enter new name: ");
+            string newName = Console.ReadLine();
+            member.modifyname(newName);
+
+        }
+        static void BuyBooks(Member member)
+        {
+            Console.Clear();
+            Console.Write("Enter book name: ");
+            string bookName = Console.ReadLine();
+            Console.Write("Enter book price: ");
+            int price = int.Parse(Console.ReadLine());
+            member.BuyBook(bookName, price);
+            Console.ReadKey();
+        }
+
+
+
+    
+    }
+
+}
