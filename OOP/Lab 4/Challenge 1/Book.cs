b3bdd58bd5a1128d54262239238f47a0ea6e5105
M Ahmed Butt <144407474+Ahmed8881@users.noreply.github.com>
2024-02-19 22:06:00 +0500

    Add files via upload

---
+++ OOP/Lab 4/Challenge 1/Book.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Challenge_1
+{
+    internal class Book
+    {
+        public string title;
+        public string[] authors;
+        public string publisher;
+        public string isbn;
+        public double price;
+        public int stock;
+       
+        public Book(string title, string[]authors, string publisher, string isbn, double price, int stock)
+        {
+            this.title = title;
+            this.authors =authors;
+            this.publisher = publisher;
+            this.isbn = isbn;
+            this.price = price;
+            this.stock = stock;
+        }
+        public void showTitle()
+        {
+            Console.WriteLine("TITLE: "+title);
+        }
+        public void showAuthor()
+        {
+            Console.WriteLine("Authors" + string.Join(";", authors));
+        }
+        public void showPublisher()
+        {
+            Console.WriteLine("PUBLISHER:"+publisher);
+        }
+        public void ShowISBN()
+        {
+            Console.WriteLine("ISBN: " + isbn);
+        }
+        public void ShowPrice()
+        {
+            Console.WriteLine("Price: $" + price);
+        }
+
+        public void ShowStock()
+        {
+            Console.WriteLine("Stock: " + stock);
+        }
+        public void Addcopies(int value)
+        {
+            stock += value;
+            Console.WriteLine(value + " are now added to the stock...");
+
+        }
+        public void SearchByTitle(string searchTitle)
+        {
+            if (title.ToLower() == searchTitle.ToLower())
+            {
+                Console.WriteLine("Book found!");
+                ShowDetails();
+            }
+            else
+            {
+                Console.WriteLine("Book not found!");
+            }
+        }
+        public void SearchByISBN(string searchISBN)
+        {
+            if (isbn == searchISBN)
+            {
+                Console.WriteLine("Book found!");
+                ShowDetails();
+            }
+            else
+            {
+                Console.WriteLine("Book not found!");
+            }
+        }
+        public void ShowDetails()
+        {
+            Console.WriteLine("Book Details:");
+            showTitle();
+            showAuthor();
+            showPublisher();
+            ShowISBN();
+            ShowPrice();
+            ShowStock();
+        }
+
+    }
+}
---
+++ OOP/Lab 4/Challenge 1/Program.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Challenge_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Book[] books = new Book[100];
+            int bookCount = 0;
+
+            bool exit = false;
+            while (!exit)
+            {
+               
+                Console.WriteLine("......Menu:................");
+                Console.WriteLine("1. Add a book");
+                Console.WriteLine("2. Search by title");
+                Console.WriteLine("3. Search by ISBN");
+                Console.WriteLine("4. Update stock");
+                Console.WriteLine("5. Exit");
+                Console.Write("Enter your choice: ");
+                int choice = int.Parse(Console.ReadLine());
+
+                if (choice == 1)
+                {
+                    Console.Clear();
+                    Console.Write("Enter title: ");
+                    string title = Console.ReadLine();
+                    Console.Write("Enter authors (comma-separated): ");
+                    string[] authors = Console.ReadLine().Split(',');
+                    Console.Write("Enter publisher: ");
+                    string publisher = Console.ReadLine();
+                    Console.Write("Enter ISBN: ");
+                    string isbn = Console.ReadLine();
+                    Console.Write("Enter price: ");
+                    double price = double.Parse(Console.ReadLine());
+                    Console.Write("Enter stock: ");
+                    int stock = int.Parse(Console.ReadLine());
+
+                    Book newBook = new Book(title, authors, publisher, isbn, price, stock);
+                    books[bookCount] = newBook;
+                    bookCount++;
+                    Console.WriteLine("Book added successfully!");
+                    Console.ReadKey();
+                    Console.Clear();
+                }
+                else if (choice == 2)
+                {
+                    Console.Clear() ;
+                    Console.Write("Enter title to search: ");
+                    string searchTitle = Console.ReadLine();
+                    foreach (Book book in books)
+                    {
+                        if (book != null)
+                        {
+                            book.SearchByTitle(searchTitle);
+                        }
+                    }
+                    Console.ReadKey() ;
+                }
+                else if (choice == 3)
+                {
+                    Console.Clear();
+                    Console.Write("Enter ISBN to search: ");
+                    string searchISBN = Console.ReadLine();
+                    foreach (Book book in books)
+                    {
+                        if (book != null)
+                        {
+                            book.SearchByISBN(searchISBN);
+                        }
+                    }
+                    Console.ReadKey() ;
+                }
+                else if (choice == 4)
+                {
+                    Console.Clear();
+                    Console.Write("Enter title of the book to update stock: ");
+                    string updateTitle = Console.ReadLine();
+                    Console.Write("Enter quantity to add: ");
+                    int quantity = int.Parse(Console.ReadLine());
+                    foreach (Book book in books)
+                    {
+                        if (book != null && book.title.ToLower() == updateTitle.ToLower())
+                        {
+                            book.Addcopies(quantity);
+                        }
+                    }
+                    Console.ReadKey() ;
+                }
+                else if (choice == 5)
+                {   
+                    exit = true;
+                }
+                else
+                {
+                    Console.WriteLine("Invalid choice! Please try again.");
+                }
+
+                Console.WriteLine();
+            }
+        }
+    }
+}
