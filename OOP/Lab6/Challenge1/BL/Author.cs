39530dea5d3eeea7ccf1ed449d5530141546fce0
M Ahmed Butt <144407474+Ahmed8881@users.noreply.github.com>
2024-02-27 19:50:34 +0500

    Add files via upload

---
+++ OOP/Lab6/Challenge1/BL/Author.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Challenge1
+{
+    internal class Author
+    {
+        private string Name; 
+        private string Email;
+        private string Gender;
+        public string GetName()
+        {
+            return Name;
+        }
+        public string GetEmail()
+        {
+            return Email;
+        }
+        public string GetGender()
+        {
+            return Gender;
+        }
+        public void SetName(string Name)
+        {
+            this.Name = Name;
+        }
+        public void SetEmail(string Email)
+        {
+            this.Email = Email;
+        }
+        public void SetGender(string Gender)
+        {
+            this.Gender= Gender;
+        }
+        public Author(string Name,string Email,string Gender )
+        {
+            this.Name= Name;
+            this.Email= Email;
+            this.Gender= Gender;
+
+        }
+
+    }
+}
---
+++ OOP/Lab6/Challenge1/BL/Book.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Challenge1
+{
+    internal class Book
+    {
+        private string BookName;
+        public string GetBookName()
+        {
+            return BookName;
+        }
+        public void SetBookName(string BookName)
+        {
+            this.BookName = BookName;
+        }
+        private Author Authors;
+        public Book(string BookName, string Name, string Email, string Gender, Author Authors)
+        {
+            this.BookName = BookName;
+            this.Authors=new Author(Authors.GetName(),Authors.GetEmail(),Authors.GetGender());
+            
+        }
+    }
+}
