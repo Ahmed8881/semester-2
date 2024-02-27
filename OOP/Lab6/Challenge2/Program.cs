b8f55b4852be32e27a86a598a9632a5fec0f3b8f
M Ahmed Butt <144407474+Ahmed8881@users.noreply.github.com>
2024-02-27 19:54:17 +0500

    Add files via upload

---
+++ OOP/Lab6/Challenge2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Challenge2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Songs Song1 = new Songs("Title1", "Ali", 1);
+            Songs Song2 = new Songs("Title2", "None", 2);
+            Songs Song3 = new Songs("Title3", "Artist", 30);
+           PlayList Playlist = new PlayList("MyPlaylist");
+            Playlist.AddSong(Song1);
+            Playlist.AddSong(Song2);
+            Playlist.AddSong(Song3);
+            Playlist.Print();
+            Playlist.CalculateDuration();
+            Console.WriteLine("Playlist Duration: " + Playlist.GetDuration());
+        }
+    }
+}
