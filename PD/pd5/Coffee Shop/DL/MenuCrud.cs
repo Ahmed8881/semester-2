836ca0fa5724955269fa925c36ae375945c60ca0
M Ahmed Butt <144407474+Ahmed8881@users.noreply.github.com>
2024-02-27 10:58:50 +0500

    Add files via upload

---
+++ PD/pd5/Coffee Shop/DL/MenuCrud.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace chlng_3_new_PD
+{
+    internal class MenuCrud
+    {
+        public static List<MenuItem> MenuItems = new List<MenuItem>();
+        public static void LoadMenuItems()
+        {
+            StreamReader File = new StreamReader("ItemsData.csv");
+            string line;
+            while ((line = File.ReadLine()) != null)
+            {
+                string[] data = line.Split(',');
+                MenuItem newItem = new MenuItem(data[0], data[1], int.Parse(data[2]));  // create menu object
+                MenuItems.Add(newItem);
+            }
+            File.Close();
+        }
+        public static MenuItem GetItemDetails(string nameOfItem)
+        {
+            foreach (MenuItem M in MenuItems)
+            {
+                if (nameOfItem == M.Name)
+                {
+                    return M;
+                }
+            }
+            return null;
+        }
+        public static void AddItem(MenuItem item)
+        {
+            MenuItems.Add(item);
+        }
+    }
+}
---
+++ PD/pd5/Coffee Shop/DL/ShopCrud.cs
@@ -0,0 +1,70 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace chlng_3_new_PD
+{
+    internal class ShopCrud
+    {
+        public static List<CoffeeShop> CoffeeShops = new List<CoffeeShop>();
+        public static void LoadShopData()
+        {
+            StreamReader File = new StreamReader("ShopData.csv");
+            string line;
+            while ((line = File.ReadLine()) != null)
+            {
+                List<MenuItem> menuItems = new List<MenuItem>();
+                string[] data = line.Split(',');
+                string[] shopItems = data[1].Split(';');
+                foreach (string item in shopItems)
+                {
+                    MenuItem temp = MenuCrud.GetItemDetails(item);
+                    if (temp != null)
+                    {
+                        menuItems.Add(temp);
+                    }
+                }
+                CoffeeShop newItem = new CoffeeShop(data[0], menuItems);  // create menu object
+                CoffeeShops.Add(newItem);
+            }
+            File.Close();
+        }
+        public static void AddShop(CoffeeShop Shop)
+        {
+            CoffeeShops.Add(Shop);
+        }
+        public static int GetShopIndex(string nameOfShop)
+        {
+            for (int i = 0; i < CoffeeShops.Count; i++)
+            {
+                if (nameOfShop == CoffeeShops[i].Name)
+                {
+                    return i;
+                }
+            }
+            return -1;
+        }
+        public static string CheapestItem(int idx)
+        {
+            return CoffeeShops[idx].CheapestItem();
+        }
+        public static string DrinkMenu(int idx)
+        {
+            return CoffeeShops[idx].DrinksOnly();
+        }
+        public static string FoodMenu(int idx)
+        {
+            return CoffeeShops[idx].FoodOnly();
+        }
+        public static bool CoffeeShopOptions(string choice)
+        {
+            if (choice == "3" || choice == "4" || choice == "4" || choice == "5" || choice == "6" || choice == "7" || choice == "8" || choice == "9")
+                return true;
+            return false;
+        }
+    }
+}
