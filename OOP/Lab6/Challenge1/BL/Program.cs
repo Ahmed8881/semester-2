327e2c4169adff867707033d5072e501f159a5f7
M Ahmed Butt <144407474+Ahmed8881@users.noreply.github.com>
2024-02-27 19:48:53 +0500

    Add files via upload

---
+++ OOP/Lab6/Challenge1/BL/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Challenge1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Author A = new Author("Ali","Emial","Male");
+            Book book = new Book("HarryPater","Ali","Email","Male",A);
+            Console.WriteLine(book.GetBookName()+A.GetName()+A.GetEmail());
+            Console.ReadLine();
+        }
+    }
+}
