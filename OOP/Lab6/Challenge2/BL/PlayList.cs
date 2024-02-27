bf5e335c50061538c2da3ebde534d964f88c044a
M Ahmed Butt <144407474+Ahmed8881@users.noreply.github.com>
2024-02-27 19:53:11 +0500

    Add files via upload

---
+++ OOP/Lab6/Challenge2/BL/PlayList.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Challenge2
+{
+    internal class PlayList
+    {
+        private string Title;
+        private List<Songs> Songs;
+        private int Duration;
+        public PlayList(string Title)
+        {
+            Title = Title;
+            Songs = new List<Songs>();
+        }
+        public void AddSong(Songs song)
+        {
+            Songs.Add(song);
+        }
+        public void RemoveSong(Songs song)
+        {
+            Songs.Remove(song);
+        }
+        public void CalculateDuration()
+        {
+            Duration = 0;
+            foreach (Songs song in Songs)
+            {
+                Duration += song.GetDuration();
+            }
+        }
+        public int GetDuration()
+        {
+            return Duration;
+        }
+        public void Print()
+        {
+            Console.WriteLine("Playlist: " + Title);
+            foreach (Songs song in Songs)
+            {
+                song.Print();
+            }
+        }
+    }
+}
---
+++ OOP/Lab6/Challenge2/BL/Songs.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Challenge2
+{
+    internal class Songs
+    {
+        private string Title;
+        private string Artist;
+        private int Duration;
+        public Songs(string title, string artist, int duration)
+        {
+            this.Title = title;
+            this.Artist = artist;
+            this.Duration = duration;
+        }
+        public string GetTitle()
+        {
+            return Title;
+        }
+        public string GetArtist()
+        {
+            return Artist;
+        }
+        public int GetDuration()
+        {
+            return Duration;
+        }
+        public void SetTitle(string title)
+        {
+            this.Title = title;
+        }
+        public void SetArtist(string artist)
+        {
+            this.Artist = artist;
+        }
+        public void SetDuration(int duration)
+        {
+            this.Duration = duration;
+        }
+        public void Print()
+        {
+            Console.WriteLine("Title: " + Title + " Artist: " + Artist + " Duration: " + Duration);
+        }
+    }
+}
