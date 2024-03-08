dfbfc37caade73000194738dd338ec4e3e7fc0db
M Ahmed Butt <144407474+Ahmed8881@users.noreply.github.com>
2024-03-08 12:57:51 +0500

    Update Program.cs

--- OOP/LAB 1/Task 3/Program.cs
+++ OOP/LAB 1/Task 3/Program.cs
@@ -1,1 +1,15 @@
 
+
+
+            students a= new students();
+            Console.WriteLine(a.sname);
+            Console.WriteLine(a.matricMarks);
+            Console.WriteLine(a.fscMarks);
+            Console.WriteLine(a.ecatMarks);
+            Console.WriteLine(a.aggregate);
+            Console.ReadKey();
+           
+        }
+    }
+}
+
