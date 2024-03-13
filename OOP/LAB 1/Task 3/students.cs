8bd524acc6bbe293a1d135da5ea29b52a5fdee4c
M Ahmed Butt <144407474+Ahmed8881@users.noreply.github.com>
2024-03-13 20:55:48 +0500

    Update students.cs

--- OOP/LAB 1/Task 3/students.cs
+++ OOP/LAB 1/Task 3/students.cs
@@ -1,1 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace practice2
+{
+    internal class students
+    {
+        //constructor
+       
+        public string sname;
+        public float matricMarks;
+        public float fscMarks;
+        public float ecatMarks;
+        public float aggregate;
+    }
+}
 
