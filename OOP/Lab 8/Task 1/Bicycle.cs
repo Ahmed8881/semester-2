bf7a42683c5a4ed021fdb8d2f8148c0bca37b6c6
M Ahmed Butt <144407474+Ahmed8881@users.noreply.github.com>
2024-04-01 04:13:30 +0500

    Add files via upload

---
+++ OOP/Lab 8/Task 1/Bicycle.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.CodeDom;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Bicycle
+    {
+        protected int cadence;
+        protected int gear;
+        protected int speed;
+
+        public Bicycle(int cadence, int grar, int speed)
+        {
+            this.cadence = cadence;
+            this.gear = grar;
+            this.speed = speed;
+
+        }
+        public void SetCadence(int cadence)
+        {
+            this.cadence = cadence;
+        }
+        public void SetGear(int gear)
+        {
+            this.gear = gear;
+        }
+        public void ApplyBracke(int decrement)
+        {
+            speed -= decrement;
+        }
+        public void SpeedUp(int increment)
+        {
+            this.speed += increment;
+        }
+        public void ShowAllinfo()
+        {
+            Console.WriteLine("Candence" + cadence);
+            Console.WriteLine("Gear" + gear);
+            Console.WriteLine("Speed" + speed);
+            
+        }
+
+    }
+}
