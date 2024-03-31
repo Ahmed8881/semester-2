8d37291a27a9e3388159cbb71fc6ec220e341687
M Ahmed Butt <144407474+Ahmed8881@users.noreply.github.com>
2024-03-31 23:27:47 +0500

    Add files via upload

---
+++ OOP/Lab 8/Task 1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            MountainBike bike = new MountainBike(5,5,7,10);
+            bike.SetGear(3);
+            bike.SetCadence(6);
+            bike.ApplyBracke(1);
+            bike.SpeedUp(5);
+            bike.ShowAllinfo();
+            Console.ReadKey();
+            
+        }
+    }
+}
